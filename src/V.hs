63365fbc36d4a307486ee15281676a4a281bab0f
Adam Sandberg Eriksson <adam@sandbergericsson.se>
2015-03-01 15:58:11 +0100

    Refactor view

--- src/V.hs
+++ src/V.hs
@@ -17,24 +17,13 @@ render v = renderText (renderView v)
 renderView :: View -> Html ()
 renderView (View{..}) =
   doctypehtml_
-    (do head_ (do meta_ [charset_ "utf-8"]
-                  meta_ [name_ "viewport"
-                        ,content_ "width=device-width, initial-scale=1"]
-                  link_ [rel_ "icon",type_ "image/png",href_ "/icon.png"]
-                  link_ [rel_ "stylesheet"
-                        ,href_ "//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css"]
-                  link_ [rel_ "stylesheet"
-                        ,href_ "//fonts.googleapis.com/css?family=Anonymous+Pro:400,700"]
-                  link_ [rel_ "stylesheet",href_ "/style.css"]
-                  title_ "Mat p\229 Campus Johanneberg")
-        body_ (do div_ [class_ "container-fluid main"]
-                       (do h1_ (toHtml date)
-                           div_ (mconcat (map renderRest restaurants))
-                           footer_ [class_ "col-xs-12 col-sm-12 col-md-12"]
-                                   (do a_ [href_ "https://github.com/adamse/mat-chalmers"]
-                                          "Kod p\229 Github"
-                                       " // Stavfel och andra konstigheter \228r n\229gon annans fel."))
-                  toHtmlRaw analytics))
+    (do sitehead
+        body_
+          (do div_ [class_ "container-fluid main"]
+                   (do h1_ (toHtml date)
+                       div_ (mconcat (map renderRest restaurants))
+                       sitefooter)
+              toHtmlRaw analytics))
 
 renderRest :: Restaurant -> Html ()
 renderRest (Restaurant{..}) =
@@ -52,12 +41,24 @@ renderMenu (Menu{..}) =
 box :: Html () -> Html ()
 box = div_ [class_ "col-xs-12 cols-sm-6 col-md-4 food"]
 
+sitehead :: Html ()
+sitehead =
+  head_ (do meta_ [charset_ "utf-8"]
+            meta_ [name_ "viewport"
+                  ,content_ "width=device-width, initial-scale=1"]
+            link_ [rel_ "icon",type_ "image/png",href_ "/icon.png"]
+            link_ [rel_ "stylesheet"
+                  ,href_ "//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css"]
+            link_ [rel_ "stylesheet"
+                  ,href_ "//fonts.googleapis.com/css?family=Anonymous+Pro:400,700"]
+            link_ [rel_ "stylesheet",href_ "/style.css"]
+            title_ "Mat p\229 Campus Johanneberg")
+
+sitefooter :: Html ()
+sitefooter =
+  footer_ [class_ "col-xs-12 col-sm-12 col-md-12"]
+          (do a_ [href_ "https://github.com/adamse/mat-chalmers"] "Kod p\229 Github"
+              " // Stavfel och andra konstigheter \228r n\229gon annans fel.")
+
 analytics :: T.Text
-analytics = "<script>\
-\(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){\
-\(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),\
-\m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)\
-\})(window,document,'script','//www.google-analytics.com/analytics.js','ga');\
-\ga('create', 'UA-60251317-1', 'auto');\
-\ga('send', 'pageview');\
-\</script>"
+analytics = "<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', 'UA-60251317-1', 'auto');ga('send', 'pageview');</script>"
