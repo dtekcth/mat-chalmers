d4c3acc95015203e11f78d940b99ea8cad6c0df1
Adam Sandberg Eriksson <adam@sandbergericsson.se>
2015-08-20 12:49:59 +0200

    add link to kortladdning

--- src/V.hs
+++ src/V.hs
@@ -58,7 +58,9 @@ sitefooter :: Html ()
 sitefooter =
   footer_ [class_ "col-xs-12 col-sm-12 col-md-12"]
           (do "Use at your own risk :) // "
-              a_ [href_ "https://github.com/adamse/mat-chalmers"] "Source at Github")
+              a_ [href_ "https://github.com/adamse/mat-chalmers"] "Source at Github"
+              " // "
+              a_ [href_ "http://kortladdning3.chalmerskonferens.se/"] "Charge your card")
 
 analytics :: T.Text
 analytics = "<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', 'UA-60251317-1', 'auto');ga('send', 'pageview');</script>"
