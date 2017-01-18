09c8dc08e890d817584f74f9a27e7139f589a088
Adam Sandberg Eriksson <adam@sandbergericsson.se>
2017-01-18 15:39:05 +0100

    Add command line options

--- src/Config.hs
+++ src/Config.hs
@@ -1,25 +1,27 @@
 {-# LANGUAGE OverloadedStrings #-}
 -- | Configuration
 
-module Config
-  ( Config()
-  , nextDayHour
-  , updateInterval
-  , servePort
-  , config
-  ) where
+module Config where
+
+import Data.Maybe
 
 -- | Configuration record
 data Config =
-  Config { nextDayHour :: Int -- ^ When to show next days menu
-         , updateInterval :: Int -- ^ Time to wait between menu updates
-         , servePort :: Int -- ^ Port to run web server on
-         }
+  Config (Maybe Int) (Maybe Int) (Maybe Int)
   deriving (Show,Read)
+
+
+
+nextDayHour :: Config -> Int -- ^ When to show next days menu
+nextDayHour (Config i _ _) = fromMaybe 14 i
+updateInterval :: Config -> Int -- ^ Time to wait between menu updates
+updateInterval (Config _ i _) = 1000000 * fromMaybe (60 * 30) i
+servePort :: Config -> Int -- ^ Port to run web server on
+servePort (Config _ _ p) = fromMaybe 5007 p
 
 -- | Default configuration
 config :: Config
 config =
-  Config 14
-         (1000000 * 60 * 60)
-         5007
+  Config Nothing
+         Nothing
+         Nothing
--- src/Main.hs
+++ src/Main.hs
@@ -1,41 +1,69 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main (main) where
 
-import Control.Concurrent
-import Control.Monad
-import Control.Monad.Trans (liftIO)
-import Data.IORef
-import Network.Wai.Middleware.Static
-import Network.Wai.Middleware.RequestLogger
-import Web.Scotty
+import           Control.Applicative (optional)
+import           Control.Concurrent
+import           Control.Monad
+import           Control.Monad.Trans (liftIO)
+import           Data.IORef
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+import           Descriptive
+import           Descriptive.Options
+import           Network.Wai.Middleware.RequestLogger
+import           Network.Wai.Middleware.Static
+import           System.Environment
+import           Text.Read (readMaybe)
+import           Web.Scotty hiding (options)
 
-import Paths_mat_chalmers
+import           Paths_mat_chalmers
 
-import Config
-import M
-import V (render)
+import           Config
+import           M
+import           V (render)
 
 main :: IO ()
 main = do
+  args <- getArgs
+  case consume options (map T.pack args) of
+    Succeeded conf -> run conf
+    Failed (Wrap (Stopped Help) _) -> do
+      putStr "mat-chalmers "
+      T.putStrLn $ textDescription (describe options [])
+    _ -> run config
+
+run conf = do
   upd <- liftIO (newMVar ()) -- putMVar when to update
-  (view, refreshAction) <- liftIO (refresh config)
-
-  -- update thread
+  (view, refreshAction) <- liftIO (refresh conf)
+  -- updater thread
   forkIO . forever $ refreshAction upd
-
   -- timer thread
-  forkIO . forever $
-    tryPutMVar upd () >> threadDelay (updateInterval config)
-
+  forkIO . forever $ tryPutMVar upd () >> threadDelay (updateInterval config)
   -- serve webpage
-  serve view upd
+  serve conf view upd
 
-serve :: IORef View -- View model
+
+data Stoppers = Help
+
+options :: Monad m => Consumer [T.Text] (Option Stoppers) m Config
+options = help *> (Config Nothing <$> interval <*> port)
+  where
+    help = stop (flag "help" "Show help" Help)
+    interval =
+      fmap (>>= (readMaybe . T.unpack))
+        (optional (arg "interval" "Update interval in seconds"))
+    port =
+      fmap (>>= (readMaybe . T.unpack))
+        (optional (arg "port" "Port to serve"))
+
+
+serve :: Config
+      -> IORef View -- View model
       -> MVar () -- Update signal
       -> IO ()
-serve view upd = do
+serve conf view upd = do
   staticDir <- getDataFileName "static"
-  scotty (servePort config) $ do
+  scotty (servePort conf) $ do
     middleware (staticPolicy (noDots >-> addBase staticDir))
     middleware logStdout
     get "/" site
