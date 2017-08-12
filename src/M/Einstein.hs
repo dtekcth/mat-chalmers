cb7c7e3434cbef88145f7efb0ad5cbe78f9984d2
Adam Sandberg Eriksson <adam@sandbergericsson.se>
2017-08-12 20:30:21 +0100

    Fix build and cabal file

--- src/M/Einstein.hs
+++ src/M/Einstein.hs
@@ -20,25 +20,27 @@ getEinstein weekday = do
   text <- handle' (get myurl)
   return $
     Restaurant myname (fromString myurl) . maybe (Left NoLunch) Right $
-    getRestaurant weekday =<< text
+    getMenus weekday =<< text
 
-getRestaurant :: Int -> T.Text -> Maybe [Menu] -- Restaurant
-getRestaurant weekday tags = do
+getMenus :: Int -> T.Text -> Maybe [Menu] -- Restaurant
+getMenus weekday tags = do
   let parts = partitions (~== "<div class='field-day'>") (parseTags tags)
   day <- safeIdx parts weekday
   return $ menus day
 
--- menus :: [Node] -> Restaurant
+menus :: [Tag T.Text] -> [Menu]
 menus day =
-  (take 4 .
-   filter (\(Menu _ spec) -> not $ T.null spec) .
-   map veg . map menu . partitions (~== "<p>") $
-   day)
+  take 4 .
+  filter (\(Menu _ spec) -> not $ T.null spec) .
+  map menu . partitions (~== "<p>") $
+  day
 
-veg m@(Menu _ spec)
+menu :: [Tag T.Text] -> Menu
+menu spec =
+  fixup $ Menu (fromString "Lunch") (T.strip . innerText . takeNext $ spec)
+
+fixup :: Menu -> Menu
+fixup m@(Menu _ spec)
   | Just suf <- T.stripPrefix (fromString "Veg:") spec =
       Menu (fromString "Vegetarisk") (T.strip suf)
   | otherwise = m
-
--- menu :: Element -> Menu
-menu spec = Menu (fromString "Lunch") (T.strip . innerText . takeNext $ spec)
