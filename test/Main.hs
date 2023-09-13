ae0cb4e9ca031c02d0b95af28845c0453fb13b9f
Jacob Jonsson <jacob.t.jonsson@gmail.com>
2023-09-13 21:13:46 +0200

    chore: remove Express restaurant because it's gone in reality
    It's good if mat reflects reality. This commit makes mat more reality
    reflective and closes #110.
    
    Thank you @Rembane for the above commit message.

--- test/Main.hs
+++ test/Main.hs
@@ -24,24 +24,6 @@ testFun expected = either
 
 main :: IO ()
 main = hspec $ do
-  describe "hasDate"
-    $ it "parses a legit date" (hasDate (BL8.pack "2/1") @?= Just (1, 2))
-  describe "The Karen Express" $ it
-    "parses a blob of JSON without error"
-    ( testFun
-        [ Menu (T.pack "Express")
-               (T.pack "Kycklinggryta, persilja, senap & kokt potatis")
-        , Menu (T.pack "Express - Vegan")
-               (T.pack "Vegetariska biffar, persiljekr\228m & potatismos")
-        ]
-
-    $ parse
-        "Swedish"
-        (BL8.pack
-          "{\"data\":{\"dishOccurrencesByTimeRange\":[{\"displayNames\":[{\"name\":\"Kycklinggryta, persilja, senap & kokt potatis\",\"categoryName\":\"Swedish\"},{\"name\":\"Chicken stew, parsley, mustard & boiled potatoes\",\"categoryName\":\"English\"}],\"startDate\":\"9/9/2019 12:00:00 AM\",\"dishType\":{\"name\":\"Express\"},\"dish\":{\"name\":\"Kycklinggryta, aubergin, zucchini, tomat & rostad potatis\"}},{\"displayNames\":[{\"name\":\"Vegetariska biffar, persiljekr\195\164m & potatismos\",\"categoryName\":\"Swedish\"},{\"name\":\"Vegetarian patties, parsley cream & mashed potatoes\",\"categoryName\":\"English\"}],\"startDate\":\"9/9/2019 12:00:00 AM\",\"dishType\":{\"name\":\"Express - Vegan\"},\"dish\":{\"name\":\"Vegetariska biffar, majonn\195\164s & potatispur\195\169\"}}]}}\n"
-        )
-    )
-
   describe "The Wijkander's"
     $ it "Parses two blobs of HTML correctly on fridays"
     $ do
