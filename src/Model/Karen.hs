7d497747317027f56419b6a1b993061b9da67949
Emily Tiberg <emily.jo.tiberg@gmail.com>
2024-05-23 10:00:04 +0200

    Fixar länk till kårrestaurangens veckomeny

--- src/Model/Karen.hs
+++ src/Model/Karen.hs
@@ -154,7 +154,7 @@ fetchAndCreateRestaurant
 fetchAndCreateRestaurant day title tag uuid =
   Restaurant
       title
-      (  "http://carbonatescreen.azurewebsites.net/menu/week/"
+      (  "https://plateimpact-screen.azurewebsites.net/menu/week/"
       <> tag
       <> "/"
       <> uuid
