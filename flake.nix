39be6697d2409d9ec793728368470693c385ad1f
pingu <nor@acorneroftheweb.com>
2024-12-21 14:18:27 +0100

    Update flake

--- flake.nix
+++ flake.nix
@@ -4,11 +4,13 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    wreq-effectful.url = "github:The1Penguin/wreq-effectful";
+    wreq-effectful.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   nixConfig.allow-import-from-derivation = true; # cabal2nix uses IFD
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, wreq-effectful }:
     let
       ghcVer = "ghc96";
       makeHaskellOverlay = overlay: final: prev: {
@@ -73,6 +75,7 @@
           let hlib = prev.haskell.lib; in
           {
             mat = hprev.callCabal2nix "mat" ./. { };
+            wreq-effectful = wreq-effectful.packages."x86_64-linux".wreq-effectful;
 
             # here's how to do hacks to the package set
             # don't run the test suite
