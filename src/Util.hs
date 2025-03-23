3638e677c15ba94a004f8b3e1c2f0f2354eaa703
pingu <nor@acorneroftheweb.com>
2025-03-23 17:45:02 +0100

    Move the swedishTimeLocale

--- src/Util.hs
+++ src/Util.hs
@@ -6,6 +6,7 @@ import           Data.ByteString.Lazy                     ( ByteString )
 import qualified Data.ByteString.Lazy          as BL
 import qualified Data.Word8                    as W8
 
+import           Data.Thyme                               ( TimeLocale (..) )
 import           Text.HTML.TagSoup                        ( Tag
                                                           , isTagText
                                                           )
@@ -33,3 +34,37 @@ removeWhitespaceTags =
 (^.^) :: Monad m => s -> Getting a s a -> m a
 (^.^)  = (pure .) . (^.)
 infixl 8 ^.^
+
+-- | Locale for Sweden
+swedishTimeLocale :: TimeLocale
+swedishTimeLocale = TimeLocale
+        { wDays =
+            [ ("Söndag",  "Sön")
+            , ("Måndag",  "Mån")
+            , ("Tisdag",  "Tis")
+            , ("Onsdag",  "Ons")
+            , ("Torsdag", "Tors")
+            , ("Fredag",  "Fre")
+            , ("Lördag",  "Lör")
+            ]
+        , months =
+            [ ("Januari",   "Jan")
+            , ("Februari",  "Feb")
+            , ("Mars",      "Mar")
+            , ("April",     "Apr")
+            , ("Maj",       "Maj")
+            , ("Juni",      "Jun")
+            , ("Juli",      "Jul")
+            , ("Augusti",   "Aug")
+            , ("September", "Sep")
+            , ("Oktober",   "Oct")
+            , ("November",  "Nov")
+            , ("December",  "Dec")
+            ]
+        , amPm = ("fm", "em")
+        , dateTimeFmt = "%a %f %e %H:%M:%S %Z %Y"
+        , dateFmt = "%d/%m/%y"
+        , timeFmt = "%H:%M:%S"
+        , time12Fmt = "%I:%M:%S %p"
+        , knownTimeZones = []
+        }
